d49e9e04a956aa2f1a2119650670e6f96f3c3072
kyBa-Le <lekyba2000hc@gmail.com>
2025-07-03 22:38:39 +0700

    (setup)/(refactor) | modify the CI flow for database check

--- auth/src/test/java/com/project/auth/domain/service/AuthServiceTest.java
+++ auth/src/test/java/com/project/auth/domain/service/AuthServiceTest.java
@@ -11,7 +11,6 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
